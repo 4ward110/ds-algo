b06b43ef16ce09a26bd6d5f9f8dcddd2b3664ca5
VuduclongPtit <vuduclong10a1cma@gmail.com>
2021-06-14 11:34:35 +0700

    add graph code

--- graph/non direct/nondirect_graph2.cpp
+++ graph/non direct/nondirect_graph2.cpp
@@ -7,27 +7,28 @@ struct dothi {
 };
 
 void docfile(dothi &g) {
-	// doc so dinh va so canh(dinh 5 canh 8)
-	FILE*f = fopen("DSC.txt","r");
+	FILE*f = fopen("DSC.txt", "r");
 	fscanf(f,"%d%d", &g.V, &g.E);
-//	printf("%d%3d",g.V, g.E);
-	// tao khung ma tran ke ( ma tran bang 0 )
-	int i,j;
-	for( i = 1; i<=g.V; i++) {
-		for( j =1; j<=g.V; j++) {
-			g.a[i][j] == 0;
+	// tao khung MTK
+	int i, j;
+	for(i = 1; i <= g.V; i++) {
+		for(j = 1; j <= g.V; j++) {
+			g.a[i][j] = 0;
 		}
 	}
-	int u,v;
-	for( i = 1; i<=g.E; i++) {
-		fscanf(f,"%d%d",&u,&v);
+	
+	// gan gia tri cho MTK
+	for(i = 1; i <= g.E; i++) {
+		int u, v;
+		fscanf(f, "%d%d", &u, &v);
+		// do thi vo huong nen a[u][v] <=> a[v][u] 
 		g.a[u][v] = g.a[v][u] = 1;
 	}
-	
+
 	fclose(f);
 }
 
-void hienMTK(dothi &g) {
+void hienMTK(dothi g) {
 	int i,j;
 	printf("%d\n", g.V);
 	for (i = 1; i<=g.V; i++) {
@@ -38,23 +39,21 @@ void hienMTK(dothi &g) {
 	}
 }
 
-void hienDSK(dothi &g) {
+void hienDSK(dothi g) {
 	int i,j;
-	printf("%d\n",g.V);
-	for(i = 1; i<= g.V; i++) {
+	for (i = 1; i<=g.V; i++) {
 		for( j = 1; j<= g.V; j++) {
-			if(g.a[i][j] == 1)
-			printf("%3d",j);
+			if(g.a[i][j] == 1) {
+				printf("%3d",j);
+			}
 		}
 		printf("\n");
 	}
 }
-
-
 int main() {
 	dothi g;
 	docfile(g);
-	hienMTK(g);
+//	hienMTK(g);
 	hienDSK(g);
 	return 0;
 }
--- graph/non direct/nondirect_graph3.cpp
+++ graph/non direct/nondirect_graph3.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <conio.h>
 #define MAX 100
 
 struct dothi {
@@ -7,34 +8,43 @@ struct dothi {
 };
 
 void docfile(dothi &g) {
-	FILE*f = fopen("MTK.txt","r");
+	int i, j;
+	i = 0;
+	char c;
+	FILE*f = fopen("DSK.txt","r");
 	fscanf(f,"%d",&g.V);
-	int i,j;
-	for(i = 1; i <= g.V; i++) {
-		for(j = 1; j <= g.V; j++) {
-			fscanf(f,"%d", &g.a[i][j]);
+	while(i <= g.V) {
+		fscanf(f, "%c", &c);
+		if(c == '\n') {
+			i++;
+		}
+		else {
+			if(c != ' ') {
+				j = c - '0';
+				g.a[i][j] =  1;
+			}
 		}
 	}
 	fclose(f);
 }
 
 void hienMTK(dothi g) {
-	int i,j;
+	int i, j;
+	printf("%d\n", g.V);
 	for(i = 1; i <= g.V; i++) {
 		for(j = 1; j <= g.V; j++) {
 			printf("%3d", g.a[i][j]);
 		}
 		printf("\n");
 	}
-	printf("\n");
 }
 
 
-int demcanh(dothi &g) {
-	int count = 0;
+int demcanh(dothi g) {
 	int i, j;
+	int count = 0;
 	for(i = 1; i <= g.V; i++) {
-		for(j = i + 1; j <= g.V; j++) {
+		for (j = i + 1; j <= g.V; j++) {
 			if(g.a[i][j] == 1) {
 				count++;
 			}
@@ -43,38 +53,24 @@ int demcanh(dothi &g) {
 	return count;
 }
 
-void hienDSC(dothi &g) {
+void hienDSC(dothi g) {
 	g.E = demcanh(g);
 	printf("%d%3d\n", g.V, g.E);
 	int i, j;
 	for(i = 1; i <= g.V; i++) {
-		for(j = i + 1; j <= g.V; j++) {
+		for( j = i + 1; j <= g.V; j++) {
 			if(g.a[i][j] == 1) {
-				printf("%d%3d\n",i, j);
+				printf("%d%3d\n", i, j);
 			}
 		}
 	}
 }
 
-
-void hienDSK(dothi &g) {
-	int i,j;
-	printf("%d\n",g.V);
-	for(i = 1; i <= g.V; i++) {
-		for(j = 1; j <= g.V; j++) {
-			if(g.a[i][j] == 1) {
-				printf("%3d",j);
-			}
-		}
-		printf("\n");
-	}
-}
 
 int main() {
 	dothi g;
 	docfile(g);
 //	hienMTK(g);
-//	hienDSC(g);
-	hienDSK(g);
+	hienDSC(g);
 	return 0;
 }
