f2a602598065c9203d17e5f5838462a8eb653146
4ward110 <vuduclong10a1cma@gmail.com>
2021-07-05 17:19:36 +0700

    add linked list

--- ktlt/danhsachke2.cpp
+++ ktlt/danhsachke2.cpp
@@ -26,7 +26,6 @@ void nhapdanhsach(DanhSach &d) {
     }
 }
 
-
 void hiendanhsach(DanhSach &d) {
     int j;
     for(j = 0; j < d.n; j++) {
@@ -62,14 +61,66 @@ void themvitrik(DanhSach &d) {
     }
 }
 
+void xoavtk(DanhSach &d) {
+    int i, k;
+    scanf("%d", &k);
+    if(k <= 0) {
+        for(i = 0; i < d.n; i++) {
+            d.sv[i] = d.sv[i+1];
+        }
+        d.n--;
+    }
+    else if (k > d.n) {
+        d.n--;
+    }
+    else {
+        for(i = k; i < d.n; i++){
+            d.sv[i] = d.sv[i+1];
+        }
+        d.n--;
+    } 
+}
 
+void tim(DanhSach d) {
+    int i, x;
+    scanf("%d", &x);
+    for(i = 0; i < d.n; i++) {
+        if(x == d.sv[i].masv) {
+            printf("%d - %s", d.sv[i].masv, d.sv[i].hoten);
+        }
+        else{
+            printf("khong co trong danh sach sv");
+            break;
+        }
+    }
+}
+
+void swap(SinhVien &a, SinhVien &b) {
+    SinhVien temp = a;
+    a = b;
+    b = temp;
+}
+
+void sapxep(DanhSach &d) {
+    int i, j, x;
+    for(i = 0; i< d.n - 1; i++) {
+        for(j = 0; j < d.n - i - 1; j++) {
+            if(d.sv[j].masv > d.sv[j+1].masv) {
+                swap(d.sv[j], d.sv[j+1]);
+            }
+        }
+    }
+}
 
 int main() {
     DanhSach d;
     nhapdanhsach(d);
     hiendanhsach(d);
-    // themvitrik(d);
-    xoak(d);
+    themvitrik(d);
+    xoavtk(d);
+    tim(d);
+    printf("-----------\n");
+    sapxep(d);
     hiendanhsach(d);
     return 0;
 }
---
+++ ktlt/danhsachlk1.cpp
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+
+typedef int SoNguyen;
+
+struct Node
+{
+    SoNguyen info;
+    Node*next;
+};
+
+struct List
+{
+    Node*dau,*cuoi;
+};
+
+void khoitao(List &l) {
+    l.dau = l.cuoi = NULL;
+}
+
+void themdau(List &l) {
+    SoNguyen x;
+    scanf("%d", &x);
+    Node*p = (Node*)malloc(sizeof(Node));
+    p->info = x;
+    p->next = NULL;
+    if(l.dau == NULL) {
+        l.dau = l.cuoi = p;
+    }
+    else {
+        p -> next = l.dau;
+        l.dau = p;
+    }
+}
+
+void themcuoi(List &l) {
+    SoNguyen x;
+    scanf("%d", &x);
+    Node*p = (Node*)malloc(sizeof(Node));
+    p->info = x;
+    p->next = NULL;
+    if(l.cuoi == NULL) {
+        l.dau = l.cuoi = p;
+    }
+    else {
+        l.cuoi -> next = p;
+        l.cuoi = p;
+    }
+}
+
+void hienList(List l) {
+    Node*i = l.dau;
+    while(i) {
+        printf("%d\t", i->info);
+        i = i->next;
+    }
+    printf("\n");
+}
+
+int demnode(List &l) {
+    Node*i = l.dau;
+    int dem = 0;
+    while(i) {
+        dem++;
+        i = i->next;
+    }
+    return dem;
+}
+
+void themvtk(List &l) {
+    SoNguyen x,k,t = demnode(l);
+    scanf("%d", &k);
+    if(k <= 0) {
+        themdau(l);
+    }
+    else if(k > t) {
+        themcuoi(l);
+    }
+    else {
+        scanf("%d", &x);
+        Node*p = l.dau;
+        Node*q = (Node*)malloc(sizeof(Node));
+        q->info=x;
+        q->next=NULL;
+        int vt = 0;
+        while(vt!=k-1 && p->next != NULL) {
+            p = p->next;
+            vt++;
+        }
+        q->next = p->next;
+        p->next = q;
+    }
+}
+
+void xoadau(List &l) {
+    Node*p = l.dau;
+    l.dau = l.dau->next;
+    free(p);
+    return;
+}
+
+void xoacuoi(List &l, int t) {
+    int k = 1;
+    Node*p = l.dau;
+    Node*q;
+    // int t = demnode(l);
+    while(p != NULL && k < t) {
+        k++;
+        q = p;
+        p = p->next;
+    }
+    q->next = p->next;
+    free(p);
+
+}
+
+
+void xoavtk(List &l) {
+    int k, t = demnode(l);
+    scanf("%d",&k);
+    if(k <= 0) {
+        xoadau(l);
+    }
+    else if(k > t) {
+        xoacuoi(l,t);
+    }
+    else {
+        Node*p = l.dau;
+        int vt = 0;
+        while(vt != k - 1 && p->next != NULL) {
+            p = p->next;
+            vt++;
+        }
+        p->next = p->next->next;
+    }
+}
+
+void huylist(List &l) {
+    Node*p = l.dau, *q;
+    while(p) {
+        q = p ->next;
+        free(p);
+        p = q;
+    }
+    l.dau = l.cuoi = NULL;
+}
+
+int main() {
+    List l;
+    khoitao(l);
+    int n,i;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++) {
+        themcuoi(l);
+    }
+    hienList(l);
+    // themvtk(l);
+    // xoavtk(l);
+    huylist(l);
+    themvtk(l);
+    hienList(l);
+
+    return 0;
+}
